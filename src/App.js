084f3ff37aadfd1b30cc8cd82b7f231dfe786cc4
Vivstoyo0082 <vivekbokoliya0082@gmail.com>
2023-01-07 23:59:16 +0530

    Adding meals component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React,{ Fragment } from "react";
+import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
+      {/* this above main is default built HTML5 element */}
     </Fragment>
   );
 }
